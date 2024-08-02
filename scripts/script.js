72815275db0c77a354ecfcae770fda23d1fea196
pikapoketon <yaposhka174@gmail.com>
2024-08-02 23:39:25 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ async function fetchPrices() {
         // Fetch data using AllOrigins to bypass CORS restrictions
         const response = await fetch(
             'https://api.allorigins.win/get?url=' +
-            encodeURIComponent('http://0.0.0.0:0000/api/data')
+            encodeURIComponent('https://pikapoketon.loca.lt//api/data')
         );
 
         if (!response.ok) {
